a69a98c51e2b07ecbdc9f8ba73ab6013995e4a20
gitpoduser01 <137901043+gitpoduser01@users.noreply.github.com>
2024-04-25 11:24:17 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -18,7 +18,7 @@ public class SeleniumTest {
     @Before
     public void setUp() {
         // Set up ChromeDriver path
-     //   System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
+      System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
 
         // Get file
         File file = new File("HelloUser.html");
